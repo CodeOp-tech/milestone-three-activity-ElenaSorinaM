48b390f02cc758c54805b1c8740218977459f4ef
Elena Manea <maneaelenasorina@gmail.com>
2023-06-03 11:38:33 +0200

    ej 4 completado

--- src/dom-manipulation/index.js
+++ src/dom-manipulation/index.js
@@ -4,7 +4,7 @@
  * 1. ✓ Cambia el estilo del elemento <body> para que todo el texto tenga una font-family: "Verdana, sans-serif".
  * 2. ✓ Asegúrate que todos los elementos <span> muestren el contenido que has introducido en los inputs.
  * 3. ✓ Haz un loop que cambie la clase de cada <li> a "list-item"
- * 5. Crea un nuevo elemento <img>, haz que el atributo src sea una imagen tuya y añádelo a la parte superior
+ * 4. ✓ Crea un nuevo elemento <img>, haz que el atributo src sea una imagen tuya y añádelo a la parte superior
  * de la sección "Sobre mi". Si necesitas añadir algún elemento adicional al index.html, puedes hacerlo,
  * pero crédito extra si lo logras sin añadir elementos adicionales.
  * 5. Bonus - Modifica y añade estilo al layout para que tu página "Sobre mi" tenga una aspecto más pulido.
@@ -38,4 +38,10 @@ document.addEventListener("DOMContentLoaded", function() {
   lis.forEach(function(elem) {
     elem.classList.add("list-item");
   });
+
+  const elemDiv = document.getElementById("picture-div");
+  const image = document.createElement("img");
+  image.src =
+    "https://images.pexels.com/photos/15879564/pexels-photo-15879564/free-photo-of-madera-barra-animal-mascota.png";
+  elemDiv.appendChild(image);
 });
